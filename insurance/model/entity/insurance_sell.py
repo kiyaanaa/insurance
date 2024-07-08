ff9d96c92e33c92602c265ddcfb08e395724a2d0
kiyana shirzadi (car) <kiyanashirzadi2003@gmail.com>
2024-07-08 18:31:00 +0330

    kiyana shirzadi and mohammad hesam sarbishe:
    version 3 : entity(insurance sell) is updated !!!

--- insurance/model/entity/insurance_sell.py
+++ insurance/model/entity/insurance_sell.py
@@ -8,17 +8,18 @@ class InsuranceSell(Base):
     _id = Column("id", Integer, ForeignKey=True, autoincrement=True)
     _person_id = Column("person_id", Integer)
     _insured_id = Column("insurance_id", Integer)
-    _start_date = Column("start_date",Date)
+    _start_date = Column("start_date", Date)
     _end_date = Column("end_date", Date)
     _date_of_sale = Column("date_of_sale", DateTime)
     _deleted = Column("deleted", Boolean, default=False)
 
-    def __init__(self, start_date, end_date, deleted=False):
+    def __init__(self, start_date, end_date, date_of_sale, deleted=False):
         self._id = None
         self._person_id = None
         self._insured_id = None
         self._start_date = start_date
         self._end_date = end_date
+        self._date_of_sale = date_of_sale
         self._deleted = deleted
 
     @property
@@ -38,6 +39,15 @@ class InsuranceSell(Base):
     @date_time_validator
     def end_date(self, value):
         self._end_date = value
+
+    @property
+    def date_of_sale(self):
+        return self._date_of_sale
+
+    @date_of_sale.setter
+    @date_time_validator
+    def date_of_sale(self, value):
+        self._date_of_sale = value
 
     @property
     def deleted(self):
